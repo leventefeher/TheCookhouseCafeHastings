d26fd333275ea260fe4ff46785948e4458e5ee4a
Mandeep Rooprai <k1516184@kcl.ac.uk>
2024-08-12 21:54:06 +0100

    Added some styles

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom";
 export const Nav = () => {
   return (
     <div className="navbar">
-      <center>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </center>
+      <ul className="flex-container">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 };
