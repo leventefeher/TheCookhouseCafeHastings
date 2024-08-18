4f1016393edcda9e2692827d183ffd2ac8672fb4
Levente Feher <l.feher@zengenti.com>
2024-08-19 00:01:12 +0100

    Adds fonts and colour to heading

--- src/components/NavBar/DesktopNavigation.jsx
+++ src/components/NavBar/DesktopNavigation.jsx
@@ -4,7 +4,11 @@ import "./NavBar.css";
 export const DesktopNavigation = () => {
   return (
     <nav className="DesktopNavigation">
-      <h2 className="logo">Logo</h2>
+      <a href="/" id="text" className="hideLinkDecoration">
+        <span id="THE">THE</span>
+        <span id="COOKHOUSE">COOKHOUSE</span>
+        <span id="CAFE">CAFÉ</span>
+      </a>
       <NavLinks />
     </nav>
   );
