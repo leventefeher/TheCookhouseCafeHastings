f438d963f57614895c2494ee34cc23b5296a7046
Mandeep Rooprai <k1516184@kcl.ac.uk>
2024-09-02 11:20:24 +0100

    Added Layout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import { Home } from "./pages/Home";
 import { Contact } from "./pages/Contact";
 import { Menu } from "./pages/Menu";
 import { NavBar } from "./components/NavBar/NavBar";
+import { Layout } from "./components/Layout";
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Layout>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
